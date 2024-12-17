de130d043c5dc8a23da0a74997de16a4d59da7b8
jinzhao <wangjingyao4@h-partners.com>
2024-12-17 19:50:09 +0800

    chore: add license header and format code style

--- harmony/react_native_screenshot_prevent/src/main/ets/RNScreenShotPreventTurboModule.ts
+++ harmony/react_native_screenshot_prevent/src/main/ets/RNScreenShotPreventTurboModule.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
 import type { TurboModuleContext,RNOHContext } from '@rnoh/react-native-openharmony/ts';
 import { TM } from "@rnoh/react-native-openharmony/generated/ts"
 import { TurboModule} from '@rnoh/react-native-openharmony/ts'
